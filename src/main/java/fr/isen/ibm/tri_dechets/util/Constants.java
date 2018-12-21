6c078751660238dc7e93ce627d959cf535f0fa69
devilbul <romaingaliay83@gmail.com>
2018-12-21 17:11:19 +0100

    Add files via upload

---
+++ src/main/java/fr/isen/ibm/tri_dechets/util/Constants.java
@@ -0,0 +1,11 @@
+package fr.isen.ibm.tri_dechets.util;
+
+public class Constants {
+
+    public static final int SELECT_PICTURE = 100;
+    public static final int TAKE_PICTURE = 101;
+
+    public static final int PERMISSION_READ_STOCKAGE = 69;
+    public static final int PERMISSION_WRITE_STOCKAGE = 6969;
+    public static final int PERMISSION_ALL = 0;
+}
